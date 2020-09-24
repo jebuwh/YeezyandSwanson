0afc71b80aef3574ac91a93cb018574dd1aa7431
Mona Lisa <email@example.com>
2020-09-24 10:00:40 -0600

    Add project files.

---
+++ YeezyandSwanson/Program.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace YeezyandSwanson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                QuoteFactory.KanyeQuote();
+                QuoteFactory.RonQuote();
+            }
+        }
+    }
+}
